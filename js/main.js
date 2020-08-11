5756da71f3dfadcf7b2d5194b1d0d10ed8c5a1ef
Miguel <miguelbogigante@gmail.com>
2020-08-11 16:49:23 +0200

    Updates

---
+++ js/main.js
@@ -0,0 +1,39 @@
+//Selecciona DOM items
+
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+
+const navItems = document.querySelectorAll(".nav-item");
+
+//Asignar el estado inicial del menu
+
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach((item) => item.classList.add("show"));
+
+    //Resetear el estado del menu
+
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach((item) => item.classList.remove("show"));
+
+    showMenu = false;
+  }
+}
+
+
+//https://www.youtube.com/watch?v=7WaohfclZRs minuto 14:21
